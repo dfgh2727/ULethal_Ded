8efcd63ccbefc94fb9e6b1e1b1e9f0750d03f5bb
dfgh2727 <dfgh2727@naver.com>
2025-08-08 00:08:26 +0900

    이수환: Enum, Instance 추가

---
+++ Lethal_Ded/Source/Lethal_Ded/Global/LCEnum.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LCEnum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API ULCEnum : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Lethal_Ded/Source/Lethal_Ded/Global/LCGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "LCGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API ULCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
