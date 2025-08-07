460f998c83d5f0705c135cdfce4544ddbf366413
dfgh2727 <dfgh2727@naver.com>
2025-08-08 00:34:00 +0900

    이수환: 게임모드 추가

---
+++ Lethal_Ded/Source/Lethal_Ded/Global/GameMode/ReadyGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/GameMode/LCGameMode.h"
+#include "ReadyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API AReadyGameMode : public ALCGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Lethal_Ded/Source/Lethal_Ded/Global/GameMode/TitleGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/GameMode/LCGameMode.h"
+#include "TitleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API ATitleGameMode : public ALCGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
