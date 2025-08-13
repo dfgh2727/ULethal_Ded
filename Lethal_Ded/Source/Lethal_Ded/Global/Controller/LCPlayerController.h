ffba7011b1a2dae9ca914777449b8ac2fd4949af
siasiaaksen <jj818181@naver.com>
2025-08-13 22:45:21 +0900

    이재영 : 캐릭터 리소스 정리 및 개발 준비 1차, LCPlayerController 생성, 캐릭터 이동 1차

---
+++ Lethal_Ded/Source/Lethal_Ded/Global/Controller/LCPlayerController.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
+#include "LCPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API ALCPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void AddMappingContext(UInputMappingContext* _MappingContext);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SetupInputComponentEvent();
+
+protected:
+	void BeginPlay() override;
+	void SetupInputComponent() override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* MappingContext = nullptr;
+	
+};
