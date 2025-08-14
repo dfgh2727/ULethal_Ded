3699c24283b820477b96ffe855bb98c605ceb59b
dfgh2727 <dfgh2727@naver.com>
2025-08-15 00:44:33 +0900

    이수환: 방 만드는 함수, 게임에 합류하는 함수 추가

--- Lethal_Ded/Source/Lethal_Ded/Global/LCGameInstance.h
+++ Lethal_Ded/Source/Lethal_Ded/Global/LCGameInstance.h
@@ -13,7 +13,32 @@ UCLASS()
 class LETHAL_DED_API ULCGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	ULCGameInstance();
+	~ULCGameInstance();
+
+	void CreateRoom();
+	void JoinRoom(FString IP, APlayerController* PlayerController);
+
+	bool CheckBootingScreenPlayed()
+	{
+		return bBootingScreenPlayed;
+	}
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Level")
+	TSoftObjectPtr<UWorld> TitleLevel;
+
+	UPROPERTY(EditAnywhere, Category = "Level")
+	TSoftObjectPtr<UWorld> PlayLevel;
+
+	UPROPERTY(EditAnywhere, Category = "Num")
+	FString Port = TEXT("7777");
+
+	bool bBootingScreenPlayed = false;
 	
 	
 	
--- Lethal_Ded/Source/Lethal_Ded/Global/LCGlobal.h
+++ Lethal_Ded/Source/Lethal_Ded/Global/LCGlobal.h
@@ -6,6 +6,7 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "LCGlobal.generated.h"
 
+class ULCGameInstance;
 /**
  * 
  */
@@ -13,8 +14,16 @@ UCLASS()
 class LETHAL_DED_API ULCGlobal : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
 	
-	
-	
+	UFUNCTION(BlueprintCallable)
+	static ULCGameInstance* GetLCGameInstance(const UWorld* World);
+
+	UFUNCTION(BlueprintCallable)
+	static void StartServer(UWorld* World, APlayerController* PlayerController);
+
+	UFUNCTION(BlueprintCallable)
+	static void ConnectServer(UWorld* World, APlayerController* PlayerController, FString& IP/*, FString& Port*/);
 	
 };
