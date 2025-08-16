edbfa7c16a7615ac2e2a6d866ee0480bb8ba8fda
dfgh2727 <dfgh2727@naver.com>
2025-08-16 21:03:13 +0900

    이수환: 컨트롤러 클래스 및 BP 추가

---
+++ Lethal_Ded/Source/Lethal_Ded/Global/DevGlob/GlobDevPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GlobDevPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API AGlobDevPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
