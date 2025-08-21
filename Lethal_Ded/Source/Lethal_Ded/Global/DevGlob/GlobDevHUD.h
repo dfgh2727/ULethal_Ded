989b21fbabd7d2af3811e4528b54ee9620857fce
dfgh2727 <dfgh2727@naver.com>
2025-08-21 20:09:48 +0900

    이수환: 테스트용 HUD 클래스 추가

---
+++ Lethal_Ded/Source/Lethal_Ded/Global/DevGlob/GlobDevHUD.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "GlobDevHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API AGlobDevHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
