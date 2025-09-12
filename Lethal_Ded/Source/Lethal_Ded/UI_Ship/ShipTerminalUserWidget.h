f32cc49f3409e9041b79a0c46970b25cead7cfa2
dfgh2727 <dfgh2727@naver.com>
2025-09-12 17:01:18 +0900

    이수환: 우주선 전용 UI 클래스 추가

---
+++ Lethal_Ded/Source/Lethal_Ded/UI_Ship/ShipTerminalUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI_Ship/ShipUserWidget.h"
+#include "ShipTerminalUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETHAL_DED_API UShipTerminalUserWidget : public UShipUserWidget
+{
+	GENERATED_BODY()
+	
+};
